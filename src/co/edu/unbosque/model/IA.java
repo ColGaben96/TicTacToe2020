6fbca03d5aed7a86c48f9a2702cf5b46c64eecbf
Charli199 <61799577+Charli199@users.noreply.github.com>
2020-03-15 13:02:45 -0500

    Actualización de la clase IA

--- src/co/edu/unbosque/model/IA.java
+++ src/co/edu/unbosque/model/IA.java
@@ -7,6 +7,11 @@ public class IA {
 	}
 	public void defender() {
 		int jugada[][] = new int [3][3];
+		int triqui[] = new int [8];
+		int esquina[] = new int[4];
+		int lados[] = new int [4];
+		int centro=5; // la variable centro guarda la posicion central del tablero
+		
 		//Se almacena las pociones del tablero del triqui en la matriz jugada.
 		jugada [0][0]=1;
 		jugada [0][1]=2;
@@ -18,6 +23,26 @@ public class IA {
 		jugada [2][1]=8;
 		jugada [2][2]=9;
 		
+		// Se alimenta el vector triqui con las posiones ganadoras de triqui
+		triqui[0]=123;
+		triqui[1]=456;
+		triqui[2]=789;
+		triqui[3]=159;
+		triqui[4]=357;
+		triqui[5]=147;
+		triqui[6]=258;
+		triqui[7]=369;
 		
+		//Se alimenta el vector esquina con las posiones esquinas del tablero
+		 esquina[0]=1;
+		 esquina[1]=3;
+		 esquina[2]=7;
+		 esquina[3]=9;
+		 
+		 //Se alimenta el vector lados con las posiciones laterales del tablero
+		 lados[0]=2;
+	     lados[1]=4;
+	     lados[2]=6;
+	     lados[3]=8;	 
 	}
 }
