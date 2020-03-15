7ab9abe069df77507675f2a020ffdaf8a3fc0636
Charli199 <61799577+Charli199@users.noreply.github.com>
2020-03-15 18:01:04 -0500

    Actualización de la clase IA

--- src/co/edu/unbosque/model/IA.java
+++ src/co/edu/unbosque/model/IA.java
@@ -3,21 +3,76 @@ package co.edu.unbosque.model;
 import java.util.Scanner;
 
 public class IA {
+	
+	int jugada[][] = new int[3][3];
+	int tablero[][] = new int[3][3];
+	int triqui[] = new int[8];
+	int esquina[] = new int[4];
+	int lados[] = new int[4];
+	int centro = 5; // la variable centro guarda la posicion central del tablero
+	int estrategia = 0;
+	int posicion = 0;
+	int posicionCpu = 0;
+	int gameOver=0;
 
 	public boolean estanAtacando() {
+		
+		int tri1=0;
+		int tri2=0;
+		int tri3=0;
+		int triquit=0;
+		
+		
+		for(int y=0; y<=2 ; y++) {
+			triquit=0;
+			for(int x=0; x<=2 ; x++) {
+				if(jugada[x][y] == 2) {
+			       triquit=triquit + jugada[x][y];
+				}
+			}
+			
+			if(triquit == 4) {
+				for(int k=0; k<=2 ; k++) {
+					if(jugada[k][y] == 0) {
+						jugada[k][y]=2;
+						System.out.print("Triqui Gané!!!!!! \n");
+						gameOver=1;
+					}else {
+						triquit=0;
+					}
+						
+				}
+			}
+		}
+		
+		for(int x=0; x<=2 ; x++) {
+			triquit=0;
+			for(int y=0; y<=2 ; y++) {
+				if(jugada[x][y] == 2) {
+			       triquit=triquit + jugada[x][y];
+				}
+			}
+			
+			if(triquit == 4) {
+				for(int yy=0; yy<=2 ; yy++) {
+					if(jugada[x][yy] == 0) {
+						jugada[x][yy]=2;
+						System.out.print("Triqui Gané!!!!!! \n");
+						gameOver=1;
+					}else {
+						triquit=0;
+					}
+						
+				}
+			}
+		}
+		
+		
 		return false;
 	}
 
 	public void defender() {
-		int jugada[][] = new int[3][3];
-		int tablero[][] = new int[3][3];
-		int triqui[] = new int[8];
-		int esquina[] = new int[4];
-		int lados[] = new int[4];
-		int centro = 5; // la variable centro guarda la posicion central del tablero
-		int estrategia = 0;
-		int posicion = 0;
-		int posicionCpu = 0;
+	
 
 		// Se almacena las pociones del tablero del triqui en la matriz tablero.
 		tablero[0][0] = 1;
@@ -68,17 +123,6 @@ public class IA {
 		System.out.print("Digite la posicion de la primera jugada: ");
 		posicion = sc.nextInt();
 
-		for (int i = 0; i <= 2; i++) {
-			for (int j = 0; j <= 2; j++) {
-
-				if (tablero[i][j] == posicion) {
-					jugada[i][j] = 1;
-				}
-
-				// System.out.print("Fila : "+ i + " Columna :" + j + "=" + jugada[i][j] +
-				// "\n");
-			}
-		}
 		for (int i = 0; i <= 3; i++) {
 
 			if (posicion == esquina[i]) {
@@ -122,16 +166,7 @@ public class IA {
 		}
 
 		}
-		for (int i = 0; i <= 2; i++) {
-			for (int j = 0; j <= 2; j++) {
 
-				if (tablero[i][j] == posicionCpu) {
-					jugada[i][j] = 2;
-				}
-
-				 System.out.print("Fila : "+ i + " Columna :" + j + "=" + jugada[i][j] +"\n");
-			}
-		}
 		System.out.print("La posicion es: " + posicion + " La estrategia es: " + estrategia);
 
 //	        ============================================= La segunda jugada =============================================
@@ -139,9 +174,275 @@ public class IA {
 		System.out.print("\n Digite la posicion de la segunda jugada: ");
 		posicion = sc.nextInt();
 
-		System.out.print("La posicion es: " + posicion + " La estrategia es: " + estrategia);
+		System.out.print("La posicion es: " + posicion + " La estrategia es: " + estrategia+ "\n");
+		
+		for (int i = 0; i <= 2; i++) {
+			for (int j = 0; j <= 2; j++) {
+
+				if (tablero[i][j] == posicion) {
+					jugada[i][j] = 1;
+				}
+
+				// System.out.print("Fila : "+ i + " Columna :" + j + "=" + jugada[i][j] + "\n");
+			}
+		}
+		int tri1=0;
+		int tri2=0;
+		int tri3=0;
+		int triquit=0;
+		
+		for(int y=0; y<=2 ; y++) {
+			triquit=0;
+			for(int x=0; x<=2 ; x++) {
+				if(jugada[x][y] != 2) {
+			       triquit=triquit + jugada[x][y];
+				}
+			}
+			
+			if(triquit == 2) {
+				for(int k=0; k<=2 ; k++) {
+					if(jugada[k][y] == 0) {
+						jugada[k][y]=2;
+					}else {
+						triquit=0;
+					}
+						
+				}
+			}
+		}
+		
+		for(int x=0; x<=2 ; x++) {
+			triquit=0;
+			for(int y=0; y<=2 ; y++) {
+				if(jugada[x][y] != 2) {
+			       triquit=triquit + jugada[x][y];
+				}
+			}
+			
+			if(triquit == 2) {
+				for(int yy=0; yy<=2 ; yy++) {
+					if(jugada[x][yy] == 0) {
+						jugada[x][yy]=2;
+					}else {
+						triquit=0;
+					}
+						
+				}
+			}
+		}
+		
+	
+		
+//        ============================================= La tercera jugada =============================================
 		
 		
 		
+		System.out.print("\n Digite la posicion de la tercera jugada: ");
+		posicion = sc.nextInt();
+
+		System.out.print("La posicion es: " + posicion + " La estrategia es: " + estrategia+ "\n");
+		
+
+		estanAtacando();
+		int jugoCpu=0; // Si es cero la cpu no ha jugado
+		
+		for(int y=0; y<=2 ; y++) {
+			triquit=0;
+			for(int x=0; x<=2 ; x++) {
+				if(jugada[x][y] != 2) {
+			       triquit=triquit + jugada[x][y];
+				}
+			}
+			
+			if(triquit == 2) {
+				for(int k=0; k<=2 ; k++) {
+					if(jugada[k][y] == 0) {
+						jugada[k][y]=2;
+						jugoCpu=1;
+					}else {
+						triquit=0;
+					}
+						
+				}
+			}
+		}
+		//============= Ciclo que bloquea el triqui del jugador =============
+		for(int x=0; x<=2 ; x++) {
+			triquit=0;
+			for(int y=0; y<=2 ; y++) {
+				if(jugada[x][y] != 2) {
+			       triquit=triquit + jugada[x][y];
+				}
+			}
+			
+			if(triquit == 2) {
+				for(int yy=0; yy<=2 ; yy++) {
+					if(jugada[x][yy] == 0) {
+						jugada[x][yy]=2;
+					}else {
+						triquit=0;
+					}
+						
+				}
+			}
+		}// fin del ciclo
+		
+		//================ Si no ha jugado la cpu realiza su mejor jugada donde exista una fila o columna que haya jugado ==================== 
+		if(jugoCpu == 0) {
+			
+			 for(int y=0; y<=2 ; y++) {
+					triquit=0;
+					for(int x=0; x<=2 ; x++) {
+						if(jugada[x][y] == 2) {
+					       triquit=triquit + jugada[x][y];
+						}
+					}
+					
+					if(triquit == 2) {
+						for(int k=0; k<=2 ; k++) {
+							if(jugada[k][y] == 0 && jugoCpu ==0)  {
+								jugada[k][y]=2;
+								jugoCpu = 1;
+							}else {
+								triquit=0;
+							}
+								
+						}
+					}
+				}
+				if(jugoCpu == 0) {
+				for(int x=0; x<=2 ; x++) {
+					triquit=0;
+					for(int y=0; y<=2 ; y++) {
+						if(jugada[x][y] == 2) {
+					       triquit=triquit + jugada[x][y];
+						}
+					}
+					
+					if(triquit == 2) {
+						for(int yy=0; yy<=2 ; yy++) {
+							if(jugada[x][yy] == 0 && jugoCpu ==0) {
+								jugada[x][yy]=2;
+								jugoCpu = 1;
+							}else {
+								triquit=0;
+							}
+								
+						}
+					}
+				}
+			}
+		} // fin jugada cpu
+		
+//      ============================================= La cuarta jugada =============================================
+		
+		
+		
+		System.out.print("\n Digite la posicion de la cuarta jugada: ");
+		posicion = sc.nextInt();
+
+		System.out.print("La posicion es: " + posicion + " La estrategia es: " + estrategia+ "\n");
+
+		estanAtacando();
+		if(gameOver == 0) {
+	    jugoCpu=0; // Si es cero la cpu no ha jugado
+		
+		for(int y=0; y<=2 ; y++) {
+			triquit=0;
+			for(int x=0; x<=2 ; x++) {
+				if(jugada[x][y] != 2) {
+			       triquit=triquit + jugada[x][y];
+				}
+			}
+			
+			if(triquit == 2) {
+				for(int k=0; k<=2 ; k++) {
+					if(jugada[k][y] == 0) {
+						jugada[k][y]=2;
+						jugoCpu=1;
+					}else {
+						triquit=0;
+					}
+						
+				}
+			}
+		}
+		//============= Ciclo que bloquea el triqui del jugador =============
+		for(int x=0; x<=2 ; x++) {
+			triquit=0;
+			for(int y=0; y<=2 ; y++) {
+				if(jugada[x][y] != 2) {
+			       triquit=triquit + jugada[x][y];
+				}
+			}
+			
+			if(triquit == 2) {
+				for(int yy=0; yy<=2 ; yy++) {
+					if(jugada[x][yy] == 0) {
+						jugada[x][yy]=2;
+					}else {
+						triquit=0;
+					}
+						
+				}
+			}
+		}// fin del ciclo
+		
+		//================ Si no ha jugado la cpu realiza su mejor jugada donde exista una fila o columna que haya jugado ==================== 
+		if(jugoCpu == 0) {
+			
+			 for(int y=0; y<=2 ; y++) {
+					triquit=0;
+					for(int x=0; x<=2 ; x++) {
+						if(jugada[x][y] == 2) {
+					       triquit=triquit + jugada[x][y];
+						}
+					}
+					
+					if(triquit == 2) {
+						for(int k=0; k<=2 ; k++) {
+							if(jugada[k][y] == 0 && jugoCpu ==0)  {
+								jugada[k][y]=2;
+								jugoCpu = 1;
+							}else {
+								triquit=0;
+							}
+								
+						}
+					}
+				}
+				if(jugoCpu == 0) {
+				for(int x=0; x<=2 ; x++) {
+					triquit=0;
+					for(int y=0; y<=2 ; y++) {
+						if(jugada[x][y] == 2) {
+					       triquit=triquit + jugada[x][y];
+						}
+					}
+					
+					if(triquit == 2) {
+						for(int yy=0; yy<=2 ; yy++) {
+							if(jugada[x][yy] == 0 && jugoCpu ==0) {
+								jugada[x][yy]=2;
+								jugoCpu = 1;
+							}else {
+								triquit=0;
+							}
+								
+						}
+					}
+				}
+			}
+		} // fin jugada cpu
+		
+	
+		}
+		for (int i = 0; i <= 2; i++) {
+			for (int j = 0; j <= 2; j++) {
+
+				 System.out.print("Fila : "+ i + " Columna :" + j + "=" + jugada[i][j] + "\n");
+			}			
+		}
+		System.out.print("GameOver");
 	}
 }
