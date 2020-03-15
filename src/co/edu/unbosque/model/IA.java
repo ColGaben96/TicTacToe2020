c6ee0b190a390bc50cbc09dd206c7ad0fec7cc7d
Charli199 <61799577+Charli199@users.noreply.github.com>
2020-03-15 12:44:10 -0500

    Actualización de la clase

--- src/co/edu/unbosque/model/IA.java
+++ src/co/edu/unbosque/model/IA.java
@@ -1,5 +1,23 @@
 package co.edu.unbosque.model;
 
 public class IA {
-
+	
+	public boolean estanAtacando() {
+		return false;
+	}
+	public void defender() {
+		int jugada[][] = new int [3][3];
+		//Se almacena las pociones del tablero del triqui en la matriz jugada.
+		jugada [0][0]=1;
+		jugada [0][1]=2;
+		jugada [0][2]=3;
+		jugada [1][0]=4;
+		jugada [1][1]=5;
+		jugada [1][2]=6;
+		jugada [2][0]=7;
+		jugada [2][1]=8;
+		jugada [2][2]=9;
+		
+		
+	}
 }
